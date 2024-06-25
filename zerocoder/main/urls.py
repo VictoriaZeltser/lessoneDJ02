1a902d089d86fddbbd0d53dae81077ff2ce44ec9
VictoriaZeltser <vikaz1976@gmail.com>
2024-06-25 23:58:29 +0300

    new code DJ02

---
+++ zerocoder/main/urls.py
@@ -0,0 +1,14 @@
+
+from django.urls import path, include
+from . import views
+
+urlpatterns = [
+
+    path('', views.index, name="home"),
+    path('new/', views.new, name='page2'),
+    path('data/', views.data, name='page3'),
+    path('test/', views.test, name='page4'),
+]
+
+
+
---
+++ zerocoder/main/views.py
@@ -0,0 +1,16 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+# Create your views here.
+
+def index(request):
+    return render(request,'main/index.html')
+
+def new(request):
+    return render(request,'main/new.html')
+
+def data(request):
+    return render(request,'main/data.html')
+
+def test(request):
+    return render(request,'main/test.html')
